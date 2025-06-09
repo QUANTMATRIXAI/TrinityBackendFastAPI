15698a77180523193641bb12e2079c10e62d287b
Abhishek160905 <abhishek@quantmatrix.ai>
2025-06-09 20:18:52 +0530

    text box

--- app/features/feature_overview/config.py
+++ app/features/feature_overview/config.py
@@ -1,24 +1,24 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import Field
+# from pydantic_settings import BaseSettings, SettingsConfigDict
+# from pydantic import Field
 
 
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
-class Settings(BaseSettings):
-    mongo_uri: str = Field(..., alias="MONGO_URI")
-    mongo_db: str = Field(..., alias="MONGO_DB")
+# class Settings(BaseSettings):
+#     mongo_uri: str = Field(..., alias="MONGO_URI")
+#     mongo_db: str = Field(..., alias="MONGO_DB")
 
-    model_config = SettingsConfigDict(
-        env_file=".env",
-        env_file_encoding="utf-8",
-        validate_default=True,
-    )
+#     model_config = SettingsConfigDict(
+#         env_file=".env",
+#         env_file_encoding="utf-8",
+#         validate_default=True,
+#     )
 
-settings = Settings()
+# settings = Settings()
 
-print("MONGO_URI:", settings.mongo_uri)
-print("MONGO_DB:", settings.mongo_db)
+# print("MONGO_URI:", settings.mongo_uri)
+# print("MONGO_DB:", settings.mongo_db)
 
 # from pydantic_settings import BaseSettings
 
--- app/features/text_box/textboxapp/main.py
+++ app/features/text_box/textboxapp/main.py
@@ -1,10 +1,3 @@
-# from fastapi import FastAPI
-# from app.api.router import api_router
-
-# app = FastAPI()
-
-# app.include_router(api_router)
-
 from fastapi import FastAPI
 from app.api.router import api_router, text_router
 
