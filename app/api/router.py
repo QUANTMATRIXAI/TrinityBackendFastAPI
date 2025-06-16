81fbcc0e9284b9b8fe0a418bc9696eabf2a96cb5
Your Name <your.email@example.com>
2025-06-16 14:52:48 +0530

    Validation atom uploaded

--- app/api/router.py
+++ app/api/router.py
@@ -2,11 +2,13 @@ from fastapi import APIRouter
 from app.features.feature_overview.endpoint import router as feature_overview_router
 from app.features.text_box.textboxapp.routes import router as textbox_router
 from app.features.chart_maker.Chart_Maker_API.app.routes import router as chart_router
+from app.features.data_upload_validate.Validate_Atom.app.routes import router as validate_router
 
 api_router = APIRouter()
 text_router  = APIRouter()
 chart_maker_router = APIRouter()
+validate_atom_router = APIRouter()
 api_router.include_router(feature_overview_router)
 text_router.include_router(textbox_router)
 chart_maker_router.include_router(chart_router)
-
+validate_atom_router.include_router(validate_router)
--- app/main.py
+++ app/main.py
@@ -6,10 +6,11 @@
 # app.include_router(api_router)
 
 from fastapi import FastAPI
-from app.api.router import api_router, text_router, chart_maker_router
+from app.api.router import api_router, text_router, chart_maker_router,validate_router
 
 app = FastAPI()
 
 app.include_router(api_router, prefix="/api")
 app.include_router(text_router, prefix="/api/t")
 app.include_router(chart_maker_router, prefix="/api/filter_charts")
+app.include_router(validate_router, prefix="/validator_atom")
